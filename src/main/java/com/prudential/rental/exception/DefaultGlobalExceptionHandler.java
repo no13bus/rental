27959169b6863fcbe96ad413d8690b20c6e91476
no13bus <no13bus@gmail.com>
2022-05-24 18:50:08 +0800

    requestparams check exception handle

--- src/main/java/com/prudential/rental/exception/DefaultGlobalExceptionHandler.java
+++ src/main/java/com/prudential/rental/exception/DefaultGlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.prudential.rental.exception;
 import com.prudential.rental.web.ApiResponse;
 import com.prudential.rental.web.BaseErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.validation.ConstraintViolationException;
 
 /**
  * @author: xxxx
@@ -19,7 +22,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @Slf4j
 public class DefaultGlobalExceptionHandler {
     @ExceptionHandler(value = {BusinessException.class})
-    @ResponseBody //在返回自定义相应类的情况下必须有，这是@ControllerAdvice注解的规定
+    @ResponseBody
     public ApiResponse defaultErrorHandler(AbstractErrorCodeException ex) {
         /*
          * 返回json数据或者String数据： 那么需要在方法上加上注解：@ResponseBody 添加return即可。
@@ -32,26 +35,38 @@ public class DefaultGlobalExceptionHandler {
         }
     }
 
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    @ResponseBody
+    public ApiResponse defaultErrorHandler(MissingServletRequestParameterException ex) {
+        return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_PARAM_CHECK.withErrorDesc(ex.getMessage()));
+    }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    @ResponseBody
+    public ApiResponse defaultErrorHandler(ConstraintViolationException ex) {
+        return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_PARAM_CHECK.withErrorDesc(ex.getMessage()));
+    }
+
     @ExceptionHandler(value = IllegalArgumentException.class)
-    @ResponseBody //在返回自定义相应类的情况下必须有，这是@ControllerAdvice注解的规定
+    @ResponseBody
     public ApiResponse defaultErrorHandler(IllegalArgumentException ex) {
         return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_PARAM_CHECK.withErrorDesc(ex.getMessage()));
     }
 
     @ExceptionHandler(value = NoHandlerFoundException.class)
-    @ResponseBody //在返回自定义相应类的情况下必须有，这是@ControllerAdvice注解的规定
+    @ResponseBody
     public ApiResponse defaultErrorHandler(NoHandlerFoundException ex) {
         return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_API_ERROR.withErrorDesc("错误的请求地址"));
     }
 
     @ExceptionHandler(value = Exception.class)
-    @ResponseBody //在返回自定义相应类的情况下必须有，这是@ControllerAdvice注解的规定
+    @ResponseBody
     public ApiResponse defaultErrorHandler(Exception ex) {
         return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_ERROR.withErrorDesc(ex.getMessage()));
     }
 
     @ExceptionHandler(value = Throwable.class)
-    @ResponseBody //在返回自定义相应类的情况下必须有，这是@ControllerAdvice注解的规定
+    @ResponseBody
     public ApiResponse defaultErrorHandler(Throwable ex) {
         return ApiResponse.buildFail(BaseErrorCode.ERR_SYSTEM_UNKNOW.withErrorDesc(ex.getMessage()));
     }
